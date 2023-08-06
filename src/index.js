b674d17e96f53594438f5e4f758c45127ccceb47
Kirill McQuillin <mcquillinkirill@gmail.com>
2023-08-06 14:49:19 -0400

    Added contact page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import generateHomePage from './pages/home.js'
 import generateMenuPage from './pages/menu.js'
+import generateContactPage from './pages/contact.js';
 
 const contentDiv = document.querySelector('.content'); // Appending everything to this
 
-contentDiv.appendChild(generateMenuPage());
+contentDiv.appendChild(generateContactPage());
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -7,5 +7,19 @@ const generateContactPage = () => {
 
     wrapperDiv.appendChild(title);
 
-    
+    const email = document.createElement('h3');
+    email.classList.add('contactEmail');
+    email.innerText = 'Email: foo@bar.com';
+
+    wrapperDiv.appendChild(email);
+
+    const phoneNumber = document.createElement('h3');
+    phoneNumber.classList.add('contactNumber');
+    phoneNumber.innerText = 'Phone Number: (111)-111-1111';
+
+    wrapperDiv.appendChild(phoneNumber);
+
+    return wrapperDiv;
 }
+
+export default generateContactPage;
